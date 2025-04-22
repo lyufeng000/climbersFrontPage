073f60a62fd6e12ca42602f3fbc3f7532e1c8b90
lyufeng000 <1191058133@qq.com>
2025-04-22 11:30:07 +0800

    问题多多

--- src/views/menberView.vue
+++ src/views/menberView.vue
@@ -2,17 +2,12 @@
   <el-container>
     <el-header>
       <el-page-header @back="goBack" content="人员组成"></el-page-header>
-      <el-button v-if="!isEditing" type="primary" @click="toggleEdit" style="float: right;">编辑</el-button>
-      <div v-else style="float: right;">
-        <el-button type="success" @click="completeChanges">完成</el-button>
-        <el-button type="warning" @click="updateEdit">修改</el-button>
-        <el-button type="danger" @click="deleteMember">删除</el-button>
-        <el-button type="primary" @click="addMember">新增</el-button>
-      </div>
+      <el-button type="primary" @click="add" class="addButton">新增</el-button>
     </el-header>
     <el-main>
       <div>
-        <el-table :data="menberTable" height="1000" border style="width: 100%" align="center" @row-click="handleRowClick" ref="memberTable">
+        <el-table :data="menberTable" height="1000" border style="width: 100%" align="center"
+                  @row-click="handleRowClick" ref="memberTable">
 
           <el-table-column type="selection" v-if="isEditing" width="100" align="center"></el-table-column>
 
@@ -20,31 +15,64 @@
 
           <el-table-column prop="office" label="职位" width="200" align="center"></el-table-column>
 
-          <el-table-column prop="category" label="部门" width="300" align="center"></el-table-column>
+          <el-table-column prop="dept" label="部门" width="300" align="center"></el-table-column>
 
           <el-table-column prop="hobby" label="爱好" align="center" width="300"></el-table-column>
 
           <el-table-column label="状态" align="center" width="300">
-            <template #default = "scope">
+            <template #default="scope">
               <span v-if="scope.row.status == true">空闲</span>
               <span v-else>忙碌</span>
             </template>
+          </el-table-column>
+
+          <el-table-column label="操作" align="center" width="150">
+            <!--一个修改超链接文字？，执行修改的函数，一个删除的文字，执行删除的函数-->
+            <el-button type="text" @click="clickUpdate(scope.row)">修改 </el-button>
+            <el-button type="text" @click="this.delete"> 删除</el-button>
           </el-table-column>
         </el-table>
       </div>
 
       <div>
-        <el-dialog title="成员信息" :visible.sync="editDialogVisible" width="50%">
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+          <span>
+            <el-form>
+              <el-form-item label="姓名">
+                <el-input v-model="menberTable.name"></el-input>
+              </el-form-item>
 
+              <el-form-item label="职位">
+                <el-input v-model="menberTable.office"></el-input>
+              </el-form-item>
+
+              <el-form-item label="部门">
+                <el-input v-model="menberTable.dept"></el-input>
+              </el-form-item>
+
+              <el-form-item label="爱好">
+                <el-input v-model="menberTable.hobby"></el-input>
+              </el-form-item>
+
+              <el-form-item label="状态">
+                <el-tooltip>
+                  <el-switch v-model="currentMember.status" active-color="#13ce66" inactive-color="#ff4949" :active-value="true" :inactive-value="false">
+                  </el-switch>
+                </el-tooltip>
+              </el-form-item>
+            </el-form>
+          </span>
+
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+          </span>
         </el-dialog>
       </div>
     </el-main>
   </el-container>
 </template>
 
-<style>
-
-</style>
 
 <script>
 import axios from "axios";
@@ -52,69 +80,74 @@ import axios from "axios";
 export default {
 
   methods: {
+    clickUpdate(row) {
+      this.dialogVisible = true;
+      this.currentMember = {...row}
+      this.update();
+    },
+
+    data() {
+      return {
+        value: false
+      }
+    },
+
     goBack() {
       this.$router.back();
     },
 
     handleRowClick(row, column, event) {
       console.log("点击行", row, column, event);
-      if (this.isEditing){
+      if (this.isEditing) {
         this.$refs.memberTable.toggleRowSelection(row);
       }
     },
 
-    //GET查
-    //POST新增
-    //PUT修改
-    //DELETE删除
-
-
-    // 获取成员数据，在网页加载时立刻执行（被mounted执行）
-    async fetchData(){
+    /*获取成员数据，在网页加载时立刻执行（被mounted执行）*/
+    async fetchData() {
       try {
         const response = await axios.get("/api/members")
-        if (response.data.code == 1){
+        if (response.data.code == 1) {
           this.menberTable = response.data.data;
-        }else {
-          console.error("后端返回失败",response.data.msg)
+        } else {
+          console.error("后端返回失败", response.data.msg)
         }
-      }catch (error){
-        console.error("成员获取失败",error)
+      } catch (error) {
+        console.error("成员获取失败", error)
       }
     },
 
-    // 编辑按钮的点击事件，反转编辑状态
-    toggleEdit() {
-      this.isEditing = !this.isEditing;
-    },
 
-    // 完成编辑按钮的点击事件
-    completeChanges() {
-      console.log("保存修改");
-      // 在此处添加保存逻辑
-      this.isEditing = false;
-    },
-
-    // 修改编辑按钮的点击事件
-    updateEdit() {
+    /*修改编辑按钮的点击事件*/
+    update() {
       console.log("修改成员");
-      // 在此处添加取消逻辑
+      // 在此处修改取消逻辑
+      // 先打开一个弹窗，弹窗里有一个表单，表单里有成员信息
+      // 通过表单获取成员信息
+      // 然后通过axios发送PUT请求，修改成员信息
+      this.dialogVisible = true;
+
 
     },
 
-    // 删除编辑按钮的点击事件
-    deleteMember() {
+    /*删除编辑按钮的点击事件*/
+    delete() {
       console.log("删除成员");
       // 在此处添加删除逻辑
     },
 
-    // 新增编辑按钮的点击事件
-    addMember() {
+    /*新增编辑按钮的点击事件*/
+    add() {
       console.log("新增成员");
       // 在此处添加新增逻辑
     },
 
-  },
+
+
+
+
+
+  },//method结束
 
   // 在组件加载时获取成员数据
   // 加载时自动运行
@@ -126,8 +159,29 @@ export default {
   data() {
     return {
       menberTable: [],
-      isEditing: false,
+      dialogVisible: false,
+      value: false,
+      currentMember: {
+        id:"",
+        name:"",
+        gender:"",
+        office:"",
+        dept:"",
+        hobby:"",
+        email:"",
+        phone:"",
+        status:""
+
+      },
     }
   }
 }
+
 </script>
+
+
+<style>
+.addButton {
+  float: right;
+}
+</style>
