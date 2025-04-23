7529bf938e3621a8c528f47386504b4bb798dc4d
lyufeng000 <1191058133@qq.com>
2025-04-23 12:02:52 +0800

    加了点注释

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,9 @@ module.exports = defineConfig({
         open: false,        // 自动打开浏览器（选填）
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://localhost:8080', // 确保后端服务地址正确
                 changeOrigin: true,
-                pathRewrite: {'^/api': ''}
+                pathRewrite: {'^/api': ''} // 确保路径重写规则正确
             }
         }
     }
