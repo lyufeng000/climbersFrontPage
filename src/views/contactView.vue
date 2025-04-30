9c6492cc3a9266567da9606ca9efc2668d7b419e
lyufeng000 <1191058133@qq.com>
2025-04-30 21:41:12 +0800

    2025/4/30

--- src/views/contactView.vue
+++ src/views/contactView.vue
@@ -4,9 +4,12 @@
   <el-container>
     <el-header><el-page-header @back = "goback" content="联系我们"></el-page-header></el-header>
     <el-main>
-      <h1>副队长：李雨峰</h1><br>
-      <h4>TEL:13908228487</h4><br>
-      <h4>mail:lyufeng000@foxmail.com</h4>
+      <el-descriptions :column=3 :data="memberTable">
+        <el-descriptions-item>
+          <template slot="label">姓名</template>
+            <div>{{ memberTable[0].name }}</div>
+        </el-descriptions-item>
+      </el-descriptions>
     </el-main>
   </el-container>
 </template>
@@ -16,11 +19,31 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
+
   methods: {
     goback() {
       this.$router.back();
+    },
+
+
+    async fetchMember() {
+      const response = await axios.get("/api/contacts");
+      this.memberTable = response.data.data;
     }
+
+  },
+
+  data() {
+    return {
+      memberTable:[],
+    }
+  },
+
+  mounted() {
+    this.fetchMember();
   }
 }
 </script>
